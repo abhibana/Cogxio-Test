39b41eba43312cd815ef77d371e87e0140566ed7
Abhishek <abhishek.bandyopadhyay@compassitesinc.com>
2015-11-28 02:22:46 +0530

    Added Tabs

--- Test/app/src/main/java/com/cogxio/android/test/activity/MainActivity.java
+++ Test/app/src/main/java/com/cogxio/android/test/activity/MainActivity.java
@@ -1,26 +1,39 @@
 package com.cogxio.android.test.activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 
 import com.cogxio.android.test.R;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout snowyTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        setLayoutElements();
+    }
+
+    private void setLayoutElements(){
+        snowyTabs = (TabLayout) findViewById(R.id.snowy_tl);
+        setUpTabs();
+    }
+
+    private void setUpTabs(){
+        String tabs[] = getResources().getStringArray(R.array.tab_names);
+        for(String tab:tabs){
+            snowyTabs.addTab(snowyTabs.newTab().setText(tab));
+        }
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
